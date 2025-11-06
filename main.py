ec3c69144a2a7fd46df199993a42f45eb1fb0918
TheCrazy8 <bennettbashford@gmail.com>
2025-11-06 16:24:19 -0500

    ee

--- main.py
+++ main.py
@@ -3,4 +3,29 @@ import random
 
 class game:
   def __init__():
-    return("placeholder")
+    enemy.init()
+    self.start_time = datetime.datetime.now()
+    self.score = 0
+    self.level = 1
+    self.is_over = False
+    self.health = 100
+    self.ticks = 0
+    self.start()
+
+  def start(self):
+    print("Game started!")
+    self.play()
+
+  def play(self):
+    while not self.is_over == true:
+      self.ticks += 1
+      if enemy.attack():
+        self.health -= enemy.damage
+
+class enemy:
+  def __init__():
+    self.damage = 10
+
+if True:
+  game.init()
+  
